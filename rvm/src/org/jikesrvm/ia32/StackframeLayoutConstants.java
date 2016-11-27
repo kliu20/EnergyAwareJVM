JikesRVM/JikesRVM

commit dac5109ab8395e8deab52f444dcbf705555e5850

RVM-1081 : Convert remaining constant interfaces to constant classes.

rvm/src/org/jikesrvm/ia32/StackframeLayoutConstants.java
@@ -140,50 +140,50 @@ import org.vmmagic.unboxed.Offset;
  *
  * </pre>
  */
-public interface StackframeLayoutConstants {
+public final class StackframeLayoutConstants {
 
-  int LOG_BYTES_IN_STACKSLOT = LOG_BYTES_IN_ADDRESS;
-  int BYTES_IN_STACKSLOT = 1 << LOG_BYTES_IN_STACKSLOT;
+  public static final int LOG_BYTES_IN_STACKSLOT = LOG_BYTES_IN_ADDRESS;
+  public static final int BYTES_IN_STACKSLOT = 1 << LOG_BYTES_IN_STACKSLOT;
 
   /** offset of caller's return address from FP */
-  Offset STACKFRAME_RETURN_ADDRESS_OFFSET = Offset.fromIntSignExtend(WORDSIZE);
+  public static final Offset STACKFRAME_RETURN_ADDRESS_OFFSET = Offset.fromIntSignExtend(WORDSIZE);
   /** base of this frame */
-  Offset STACKFRAME_FRAME_POINTER_OFFSET = Offset.zero();
+  public static final Offset STACKFRAME_FRAME_POINTER_OFFSET = Offset.zero();
   /** offset of method id from FP */
-  Offset STACKFRAME_METHOD_ID_OFFSET = Offset.fromIntSignExtend(-WORDSIZE);
+  public static final Offset STACKFRAME_METHOD_ID_OFFSET = Offset.fromIntSignExtend(-WORDSIZE);
   /** offset of work area from FP */
-  Offset STACKFRAME_BODY_OFFSET = Offset.fromIntSignExtend(-2 * WORDSIZE);
+  public static final Offset STACKFRAME_BODY_OFFSET = Offset.fromIntSignExtend(-2 * WORDSIZE);
   /** size of frame header, in bytes */
-  int STACKFRAME_HEADER_SIZE = 3 * WORDSIZE;
+  public static final int STACKFRAME_HEADER_SIZE = 3 * WORDSIZE;
 
   /** space to save entire FPU state.  The FPU state is saved only for 'bridge' frames */
-  int FPU_STATE_SIZE = 108;
+  public static final int FPU_STATE_SIZE = 108;
   /** Currently only use the low 8 bytes, only use 4 SSE2 params */
-  int XMM_STATE_SIZE = 8 * 4;
+  public static final int XMM_STATE_SIZE = 8 * 4;
 
   /** fp value indicating end of stack walkback */
-  Address STACKFRAME_SENTINEL_FP = Address.fromIntSignExtend(-2);
+  public static final Address STACKFRAME_SENTINEL_FP = Address.fromIntSignExtend(-2);
   /** marker for "assembler" frames that have no associated RVMMethod */
-  int INVISIBLE_METHOD_ID = -1;
+  public static final int INVISIBLE_METHOD_ID = -1;
 
   // Stackframe alignment.
-  // Align to 8 byte boundary for good floating point save/restore performance (on powerPC, anyway).
+  // Align to 8 byte boundary for good floating popublic static final int save/restore performance (on powerPC, anyway).
   //
-  int STACKFRAME_ALIGNMENT = 8;
+  public static final int STACKFRAME_ALIGNMENT = 8;
 
   // Sizes for stacks and subregions thereof.
   // Values are in bytes and must be a multiple of WORDSIZE (size of a stack slot).
   //
   /** how much to grow stack when overflow detected */
-  int STACK_SIZE_GROW = (VM.BuildFor64Addr ? 16 : 8) * 1024;
+  public static final int STACK_SIZE_GROW = (VM.BuildFor64Addr ? 16 : 8) * 1024;
   /** max space needed for stack overflow trap processing */
-  int STACK_SIZE_GUARD = 64 * 1024;
+  public static final int STACK_SIZE_GUARD = 64 * 1024;
   /** max space needed for any native code called by vm */
-  int STACK_SIZE_SYSCALL = (VM.BuildFor64Addr ? 8 : 4) * 1024;
+  public static final int STACK_SIZE_SYSCALL = (VM.BuildFor64Addr ? 8 : 4) * 1024;
   /** max space needed for dlopen sys call */
-  int STACK_SIZE_DLOPEN = 30 * 1024;
+  public static final int STACK_SIZE_DLOPEN = 30 * 1024;
   /** max space needed while running with gc disabled */
-  int STACK_SIZE_GCDISABLED = (VM.BuildFor64Addr ? 8 : 4) * 1024;
+  public static final int STACK_SIZE_GCDISABLED = (VM.BuildFor64Addr ? 8 : 4) * 1024;
 
    // Complications:
    // - STACK_SIZE_GUARD must be greater than STACK_SIZE_NATIVE or STACK_SIZE_GCDISABLED
@@ -199,23 +199,27 @@ public interface StackframeLayoutConstants {
    //
 
   /** initial stack space to allocate for normal thread (includes guard region) */
-  int STACK_SIZE_NORMAL =
+  public static final int STACK_SIZE_NORMAL =
       STACK_SIZE_GUARD +
       STACK_SIZE_GCDISABLED +
       200 * 1024;
   /** total stack space to allocate for boot thread (includes guard region) */
-  int STACK_SIZE_BOOT =
+  public static final int STACK_SIZE_BOOT =
       STACK_SIZE_GUARD +
       STACK_SIZE_GCDISABLED +
       30 * 1024;
   /** total stack space to allocate for collector thread (includes guard region) */
-  int STACK_SIZE_COLLECTOR =
+  public static final int STACK_SIZE_COLLECTOR =
       STACK_SIZE_GUARD +
       STACK_SIZE_GCDISABLED +
       20 * 1024;
   /** upper limit on stack size (includes guard region) */
-  int STACK_SIZE_MAX =
+  public static final int STACK_SIZE_MAX =
       STACK_SIZE_GUARD + STACK_SIZE_GCDISABLED + 200 * 1024;
 
-  int STACK_SIZE_JNINATIVE_GROW = 0; // TODO!!;
+  public static final int STACK_SIZE_JNINATIVE_GROW = 0; // TODO!!;
+
+  private StackframeLayoutConstants() {
+    // prevent instantiation
+  }
 }

rvm/src/org/jikesrvm/ppc/StackframeLayoutConstants.java
@@ -128,51 +128,51 @@ import org.vmmagic.unboxed.Offset;
  *
  * </pre>
  */
-public interface StackframeLayoutConstants {
+public final class StackframeLayoutConstants {
 
-  int LOG_BYTES_IN_STACKSLOT = LOG_BYTES_IN_ADDRESS;
-  int BYTES_IN_STACKSLOT = 1 << LOG_BYTES_IN_STACKSLOT;
+  public static final int LOG_BYTES_IN_STACKSLOT = LOG_BYTES_IN_ADDRESS;
+  public static final int BYTES_IN_STACKSLOT = 1 << LOG_BYTES_IN_STACKSLOT;
 
   /** size of frame header, in bytes */
-  int STACKFRAME_HEADER_SIZE = 3 * BYTES_IN_STACKSLOT;
+  public static final int STACKFRAME_HEADER_SIZE = 3 * BYTES_IN_STACKSLOT;
 
   // SVR4 ABI has no space between FP and LR, swap the positions for LR and CMID depending on ABI.
-  Offset STACKFRAME_RETURN_ADDRESS_OFFSET = Offset.fromIntSignExtend(VM.BuildForPower64ELF_ABI ? 2 * BYTES_IN_STACKSLOT : BYTES_IN_STACKSLOT);
-  Offset STACKFRAME_METHOD_ID_OFFSET = Offset.fromIntSignExtend(VM.BuildForPower64ELF_ABI ? BYTES_IN_STACKSLOT : 2 * BYTES_IN_STACKSLOT);
+  public static final Offset STACKFRAME_RETURN_ADDRESS_OFFSET = Offset.fromIntSignExtend(VM.BuildForPower64ELF_ABI ? 2 * BYTES_IN_STACKSLOT : BYTES_IN_STACKSLOT);
+  public static final Offset STACKFRAME_METHOD_ID_OFFSET = Offset.fromIntSignExtend(VM.BuildForPower64ELF_ABI ? BYTES_IN_STACKSLOT : 2 * BYTES_IN_STACKSLOT);
 
   /** base of this frame **/
-  Offset STACKFRAME_FRAME_POINTER_OFFSET = Offset.zero();
+  public static final Offset STACKFRAME_FRAME_POINTER_OFFSET = Offset.zero();
 
   /** fp value indicating end of stack walkback */
-  Address STACKFRAME_SENTINEL_FP = Address.fromIntSignExtend(-2);
+  public static final Address STACKFRAME_SENTINEL_FP = Address.fromIntSignExtend(-2);
   /** marker for "assembler" frames that have no associated RVMMethod **/
-  int INVISIBLE_METHOD_ID = -1;
+  public static final int INVISIBLE_METHOD_ID = -1;
 
   /**
    * Stackframe alignment. Align to 8 byte boundary for good floating point
    * save/restore performance (on powerPC, anyway).
    */
-  int STACKFRAME_ALIGNMENT = BYTES_IN_DOUBLE;
+  public static final int STACKFRAME_ALIGNMENT = BYTES_IN_DOUBLE;
 
   // Sizes for stacks and sub-regions thereof.
   // Values are in bytes and must be a multiple of 8 (size of a stack slot on 64-architecture).
 
   /** how much to grow normal stack when overflow detected */
-  int STACK_SIZE_GROW = 8 * 1024;
+  public static final int STACK_SIZE_GROW = 8 * 1024;
   /** max space needed for stack overflow trap processing */
-  int STACK_SIZE_GUARD = 8 * 1024;
+  public static final int STACK_SIZE_GUARD = 8 * 1024;
   /** max space needed for entry to sysCall# via Magic */
-  int STACK_SIZE_NATIVE = 4 * 1024;
+  public static final int STACK_SIZE_NATIVE = 4 * 1024;
   /** max space needed for first entry to native code via JNI */
-  int STACK_SIZE_JNINATIVE = 180 * 1024;
+  public static final int STACK_SIZE_JNINATIVE = 180 * 1024;
   /** max space needed for dlopen sys call */
-  int STACK_SIZE_DLOPEN = 30 * 1024;
+  public static final int STACK_SIZE_DLOPEN = 30 * 1024;
   /** size to grow once for native on first entry via JNI */
-  int STACK_SIZE_JNINATIVE_GROW = 184 * 1024;
+  public static final int STACK_SIZE_JNINATIVE_GROW = 184 * 1024;
   /** max space needed while running with gc disabled */
-  int STACK_SIZE_GCDISABLED = 4 * 1024; //
+  public static final int STACK_SIZE_GCDISABLED = 4 * 1024; //
   /** upper limit on stack size (includes guard region) */
-  int STACK_SIZE_MAX = 512 * 1024;
+  public static final int STACK_SIZE_MAX = 512 * 1024;
 
   // Complications:
   // - STACK_SIZE_GUARD must be greater than STACK_SIZE_NATIVE or STACK_SIZE_GCDISABLED
@@ -187,13 +187,14 @@ public interface StackframeLayoutConstants {
   // Initial stack sizes. Note that stacks for collector threads cannot grow.
 
   /** Initial stack size for normal thread. Stacks for "normal" threads grow as needed by trapping on guard region. */
-  int STACK_SIZE_NORMAL = STACK_SIZE_GUARD + STACK_SIZE_GCDISABLED + 16 * 1024 + (VM.BuildFor64Addr ? 256 * 1024 : 0);
+  public static final int STACK_SIZE_NORMAL = STACK_SIZE_GUARD + STACK_SIZE_GCDISABLED + 16 * 1024 + (VM.BuildFor64Addr ? 256 * 1024 : 0);
   /** Initial stack size for boot thread. Stacks for "boot" thread grow as needed - boot thread calls JNI during initialization */
-  int STACK_SIZE_BOOT = STACK_SIZE_GUARD + STACK_SIZE_GCDISABLED + STACK_SIZE_JNINATIVE + 128 * 1024;
+  public static final int STACK_SIZE_BOOT = STACK_SIZE_GUARD + STACK_SIZE_GCDISABLED + STACK_SIZE_JNINATIVE + 128 * 1024;
   /** Initial stack size for collector thread. Stacks for "collector" threads are fixed in size and cannot grow. */
-  int STACK_SIZE_COLLECTOR = STACK_SIZE_GUARD + STACK_SIZE_GCDISABLED + 32 * 1024;
+  public static final int STACK_SIZE_COLLECTOR = STACK_SIZE_GUARD + STACK_SIZE_GCDISABLED + 32 * 1024;
+
+  private StackframeLayoutConstants() {
+    // prevent instantiation
+  }
 
 }
-
-
-

rvm/src-generated/ia32-assembler/Assembler.in
@@ -187,7 +187,7 @@ import org.vmmagic.unboxed.*;
  * @see ForwardReference
  *
 */
-public class Assembler extends AbstractAssembler implements RegisterConstants {
+public class Assembler extends AbstractAssembler {
 
   /**
    * The lister object is used to print generated machine code.

rvm/src/org/jikesrvm/compilers/baseline/ia32/Barriers.java
@@ -12,21 +12,25 @@
  */
 package org.jikesrvm.compilers.baseline.ia32;
 
+import static org.jikesrvm.ia32.BaselineConstants.S0;
+import static org.jikesrvm.ia32.BaselineConstants.SP;
+import static org.jikesrvm.ia32.BaselineConstants.T0;
+
 import org.jikesrvm.Configuration;
 import org.jikesrvm.classloader.MethodReference;
 import org.jikesrvm.classloader.NormalMethod;
 import org.jikesrvm.compilers.common.assembler.ia32.Assembler;
-import org.jikesrvm.ia32.BaselineConstants;
+import org.jikesrvm.ia32.RegisterConstants.GPR;
 import org.jikesrvm.runtime.Entrypoints;
-import org.vmmagic.unboxed.Offset;
 import org.vmmagic.pragma.Inline;
+import org.vmmagic.unboxed.Offset;
 
 /**
  * Class called from baseline compiler to generate architecture specific
  * write barriers for garbage collectors.  For baseline
  * compiled methods, the write barrier calls methods of WriteBarrier.
  */
-class Barriers implements BaselineConstants {
+class Barriers {
 
   /**
    * Generate code to perform an array store barrier. On entry the stack holds:

rvm/src/org/jikesrvm/compilers/baseline/ia32/BaselineCompilerImpl.java
@@ -15,6 +15,50 @@ package org.jikesrvm.compilers.baseline.ia32;
 import static org.jikesrvm.classloader.ClassLoaderConstants.CP_CLASS;
 import static org.jikesrvm.classloader.ClassLoaderConstants.CP_STRING;
 import static org.jikesrvm.compilers.common.assembler.ia32.AssemblerConstants.*;
+import static org.jikesrvm.ia32.ArchConstants.SSE2_BASE;
+import static org.jikesrvm.ia32.ArchConstants.SSE2_FULL;
+import static org.jikesrvm.ia32.BaselineConstants.EBP_SAVE_OFFSET;
+import static org.jikesrvm.ia32.BaselineConstants.EBX_SAVE_OFFSET;
+import static org.jikesrvm.ia32.BaselineConstants.EDI_SAVE_OFFSET;
+import static org.jikesrvm.ia32.BaselineConstants.FPU_SAVE_OFFSET;
+import static org.jikesrvm.ia32.BaselineConstants.LG_WORDSIZE;
+import static org.jikesrvm.ia32.BaselineConstants.S0;
+import static org.jikesrvm.ia32.BaselineConstants.S1;
+import static org.jikesrvm.ia32.BaselineConstants.SAVED_GPRS;
+import static org.jikesrvm.ia32.BaselineConstants.SAVED_GPRS_FOR_SAVE_LS_REGISTERS;
+import static org.jikesrvm.ia32.BaselineConstants.SP;
+import static org.jikesrvm.ia32.BaselineConstants.T0;
+import static org.jikesrvm.ia32.BaselineConstants.T0_SAVE_OFFSET;
+import static org.jikesrvm.ia32.BaselineConstants.T1;
+import static org.jikesrvm.ia32.BaselineConstants.T1_SAVE_OFFSET;
+import static org.jikesrvm.ia32.BaselineConstants.TR;
+import static org.jikesrvm.ia32.BaselineConstants.WORDSIZE;
+import static org.jikesrvm.ia32.BaselineConstants.XMM_SAVE_OFFSET;
+import static org.jikesrvm.ia32.RegisterConstants.EAX;
+import static org.jikesrvm.ia32.RegisterConstants.EBP;
+import static org.jikesrvm.ia32.RegisterConstants.EBX;
+import static org.jikesrvm.ia32.RegisterConstants.ECX;
+import static org.jikesrvm.ia32.RegisterConstants.EDI;
+import static org.jikesrvm.ia32.RegisterConstants.EDX;
+import static org.jikesrvm.ia32.RegisterConstants.ESI;
+import static org.jikesrvm.ia32.RegisterConstants.ESP;
+import static org.jikesrvm.ia32.RegisterConstants.FP0;
+import static org.jikesrvm.ia32.RegisterConstants.FP1;
+import static org.jikesrvm.ia32.RegisterConstants.NATIVE_PARAMETER_FPRS;
+import static org.jikesrvm.ia32.RegisterConstants.NATIVE_PARAMETER_GPRS;
+import static org.jikesrvm.ia32.RegisterConstants.NONVOLATILE_GPRS;
+import static org.jikesrvm.ia32.RegisterConstants.NUM_PARAMETER_FPRS;
+import static org.jikesrvm.ia32.RegisterConstants.NUM_PARAMETER_GPRS;
+import static org.jikesrvm.ia32.RegisterConstants.THREAD_REGISTER;
+import static org.jikesrvm.ia32.RegisterConstants.XMM0;
+import static org.jikesrvm.ia32.RegisterConstants.XMM1;
+import static org.jikesrvm.ia32.RegisterConstants.XMM2;
+import static org.jikesrvm.ia32.RegisterConstants.XMM3;
+import static org.jikesrvm.ia32.StackframeLayoutConstants.FPU_STATE_SIZE;
+import static org.jikesrvm.ia32.StackframeLayoutConstants.STACKFRAME_BODY_OFFSET;
+import static org.jikesrvm.ia32.StackframeLayoutConstants.STACKFRAME_HEADER_SIZE;
+import static org.jikesrvm.ia32.StackframeLayoutConstants.STACKFRAME_METHOD_ID_OFFSET;
+import static org.jikesrvm.ia32.StackframeLayoutConstants.XMM_STATE_SIZE;
 import static org.jikesrvm.ia32.TrapConstants.RVM_TRAP_BASE;
 import static org.jikesrvm.mm.mminterface.Barriers.*;
 import static org.jikesrvm.objectmodel.JavaHeaderConstants.ARRAY_LENGTH_BYTES;
@@ -22,13 +66,13 @@ import static org.jikesrvm.objectmodel.TIBLayoutConstants.NEEDS_DYNAMIC_LINK;
 import static org.jikesrvm.objectmodel.TIBLayoutConstants.TIB_DOES_IMPLEMENT_INDEX;
 import static org.jikesrvm.objectmodel.TIBLayoutConstants.TIB_INTERFACE_DISPATCH_TABLE_INDEX;
 import static org.jikesrvm.objectmodel.TIBLayoutConstants.TIB_SUPERCLASS_IDS_INDEX;
-import static org.jikesrvm.runtime.RuntimeEntrypoints.TRAP_UNREACHABLE_BYTECODE;
 import static org.jikesrvm.runtime.JavaSizeConstants.BYTES_IN_BYTE;
 import static org.jikesrvm.runtime.JavaSizeConstants.BYTES_IN_INT;
 import static org.jikesrvm.runtime.JavaSizeConstants.BYTES_IN_LONG;
 import static org.jikesrvm.runtime.JavaSizeConstants.BYTES_IN_SHORT;
 import static org.jikesrvm.runtime.JavaSizeConstants.LOG_BYTES_IN_INT;
 import static org.jikesrvm.runtime.JavaSizeConstants.LOG_BYTES_IN_SHORT;
+import static org.jikesrvm.runtime.RuntimeEntrypoints.TRAP_UNREACHABLE_BYTECODE;
 import static org.jikesrvm.runtime.UnboxedSizeConstants.LOG_BYTES_IN_ADDRESS;
 
 import org.jikesrvm.VM;
@@ -55,7 +99,8 @@ import org.jikesrvm.compilers.common.CompiledMethod;
 import org.jikesrvm.compilers.common.assembler.AbstractAssembler;
 import org.jikesrvm.compilers.common.assembler.ForwardReference;
 import org.jikesrvm.compilers.common.assembler.ia32.Assembler;
-import org.jikesrvm.ia32.BaselineConstants;
+import org.jikesrvm.ia32.RegisterConstants.GPR;
+import org.jikesrvm.ia32.RegisterConstants.XMM;
 import org.jikesrvm.jni.ia32.JNICompiler;
 import org.jikesrvm.mm.mminterface.MemoryManager;
 import org.jikesrvm.objectmodel.ObjectModel;
@@ -73,7 +118,7 @@ import org.vmmagic.unboxed.Offset;
 /**
  * BaselineCompilerImpl is the baseline compiler implementation for the IA32 architecture.
  */
-public final class BaselineCompilerImpl extends BaselineCompiler implements BaselineConstants {
+public final class BaselineCompilerImpl extends BaselineCompiler {
 
   private final Assembler asm;
 

rvm/src/org/jikesrvm/compilers/baseline/ia32/BaselineExceptionDeliverer.java
@@ -13,6 +13,15 @@
 package org.jikesrvm.compilers.baseline.ia32;
 
 import static org.jikesrvm.VM.NOT_REACHED;
+import static org.jikesrvm.ia32.BaselineConstants.EBP_SAVE_OFFSET;
+import static org.jikesrvm.ia32.BaselineConstants.EBX_SAVE_OFFSET;
+import static org.jikesrvm.ia32.BaselineConstants.EDI_SAVE_OFFSET;
+import static org.jikesrvm.ia32.BaselineConstants.SAVED_GPRS;
+import static org.jikesrvm.ia32.BaselineConstants.SP;
+import static org.jikesrvm.ia32.RegisterConstants.EBP;
+import static org.jikesrvm.ia32.RegisterConstants.EBX;
+import static org.jikesrvm.ia32.RegisterConstants.EDI;
+import static org.jikesrvm.ia32.StackframeLayoutConstants.STACK_SIZE_GUARD;
 import static org.jikesrvm.runtime.UnboxedSizeConstants.BYTES_IN_ADDRESS;
 
 import org.jikesrvm.VM;
@@ -20,7 +29,6 @@ import org.jikesrvm.architecture.AbstractRegisters;
 import org.jikesrvm.classloader.NormalMethod;
 import org.jikesrvm.compilers.baseline.BaselineCompiledMethod;
 import org.jikesrvm.compilers.common.CompiledMethod;
-import org.jikesrvm.ia32.BaselineConstants;
 import org.jikesrvm.objectmodel.ObjectModel;
 import org.jikesrvm.runtime.ExceptionDeliverer;
 import org.jikesrvm.runtime.Magic;
@@ -33,7 +41,7 @@ import org.vmmagic.unboxed.Offset;
  * Handle exception delivery and stack unwinding for methods compiled by
  * baseline compiler.
  */
-public final class BaselineExceptionDeliverer extends ExceptionDeliverer implements BaselineConstants {
+public final class BaselineExceptionDeliverer extends ExceptionDeliverer {
 
   /**
    * Pass control to a catch block.

rvm/src/org/jikesrvm/compilers/baseline/ia32/BaselineGCMapIterator.java
@@ -12,6 +12,21 @@
  */
 package org.jikesrvm.compilers.baseline.ia32;
 
+import static org.jikesrvm.ia32.BaselineConstants.BRIDGE_FRAME_EXTRA_SIZE;
+import static org.jikesrvm.ia32.BaselineConstants.EBP_SAVE_OFFSET;
+import static org.jikesrvm.ia32.BaselineConstants.EBX_SAVE_OFFSET;
+import static org.jikesrvm.ia32.BaselineConstants.EDI_SAVE_OFFSET;
+import static org.jikesrvm.ia32.BaselineConstants.LG_WORDSIZE;
+import static org.jikesrvm.ia32.BaselineConstants.STACKFRAME_FIRST_PARAMETER_OFFSET;
+import static org.jikesrvm.ia32.BaselineConstants.T0;
+import static org.jikesrvm.ia32.BaselineConstants.T0_SAVE_OFFSET;
+import static org.jikesrvm.ia32.BaselineConstants.T1;
+import static org.jikesrvm.ia32.BaselineConstants.T1_SAVE_OFFSET;
+import static org.jikesrvm.ia32.BaselineConstants.WORDSIZE;
+import static org.jikesrvm.ia32.RegisterConstants.EBP;
+import static org.jikesrvm.ia32.RegisterConstants.EBX;
+import static org.jikesrvm.ia32.RegisterConstants.EDI;
+import static org.jikesrvm.ia32.RegisterConstants.NUM_PARAMETER_GPRS;
 import static org.jikesrvm.runtime.UnboxedSizeConstants.BYTES_IN_ADDRESS;
 
 import org.jikesrvm.VM;
@@ -22,14 +37,13 @@ import org.jikesrvm.compilers.baseline.BaselineCompiledMethod;
 import org.jikesrvm.compilers.baseline.ReferenceMaps;
 import org.jikesrvm.compilers.common.CompiledMethod;
 import org.jikesrvm.compilers.common.CompiledMethods;
-import org.jikesrvm.ia32.BaselineConstants;
 import org.jikesrvm.mm.mminterface.GCMapIterator;
 import org.jikesrvm.runtime.DynamicLink;
 import org.jikesrvm.runtime.Magic;
 import org.vmmagic.pragma.Uninterruptible;
 import org.vmmagic.unboxed.Address;
-import org.vmmagic.unboxed.Offset;
 import org.vmmagic.unboxed.AddressArray;
+import org.vmmagic.unboxed.Offset;
 
 /**
  * Iterator for stack frame  built by the Baseline compiler.
@@ -40,7 +54,7 @@ import org.vmmagic.unboxed.AddressArray;
  * java stack for the stack frame.
  */
 @Uninterruptible
-public final class BaselineGCMapIterator extends GCMapIterator implements BaselineConstants {
+public final class BaselineGCMapIterator extends GCMapIterator {
   private static final boolean TRACE_ALL = false;
   private static final boolean TRACE_DL = false; // dynamic link frames
 

rvm/src/org/jikesrvm/compilers/baseline/ppc/Barriers.java
@@ -12,11 +12,18 @@
  */
 package org.jikesrvm.compilers.baseline.ppc;
 
+import static org.jikesrvm.ppc.BaselineConstants.F0;
+import static org.jikesrvm.ppc.BaselineConstants.S0;
+import static org.jikesrvm.ppc.BaselineConstants.T0;
+import static org.jikesrvm.ppc.BaselineConstants.T1;
+import static org.jikesrvm.ppc.BaselineConstants.T2;
+import static org.jikesrvm.ppc.BaselineConstants.T3;
+import static org.jikesrvm.ppc.BaselineConstants.T4;
+
 import org.jikesrvm.VM;
 import org.jikesrvm.classloader.MethodReference;
 import org.jikesrvm.classloader.NormalMethod;
 import org.jikesrvm.compilers.common.assembler.ppc.Assembler;
-import org.jikesrvm.ppc.BaselineConstants;
 import org.jikesrvm.runtime.Entrypoints;
 import org.vmmagic.unboxed.Offset;
 
@@ -25,7 +32,7 @@ import org.vmmagic.unboxed.Offset;
  * write barrier for generational garbage collectors.  For baseline
  * compiled methods, the write barrier calls methods of WriteBarrier.
  */
-class Barriers implements BaselineConstants {
+class Barriers {
 
   // on entry java stack contains ...|array_ref|index|value|
   static void compileArrayStoreBarrier(BaselineCompilerImpl comp) {

kliu20/EnergyAwareJVM

commit 06b40e42c8885683cab00cffda9eb48bef7b66c1

Fix documentation error that I introduced in dac5109ab8395e8deab52f444dcbf705555e5850 .

rvm/src/org/jikesrvm/ia32/StackframeLayoutConstants.java
@@ -167,7 +167,7 @@ public final class StackframeLayoutConstants {
   public static final int INVISIBLE_METHOD_ID = -1;
 
   // Stackframe alignment.
-  // Align to 8 byte boundary for good floating popublic static final int save/restore performance (on powerPC, anyway).
+  // Align to 8 byte boundary for good floating point save/restore performance (on powerPC, anyway).
   //
   public static final int STACKFRAME_ALIGNMENT = 8;
 