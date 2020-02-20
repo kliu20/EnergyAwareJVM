d773d0e7311627f669dc07727f5dbd4843f736ed
Kenan Liu <kliu20@binghamton.edu>
2020-02-20 15:50:58 -0500

    Timestamp and Thread ID Implemented

--- tools/bootloader/log_queue.c
+++ tools/bootloader/log_queue.c
@@ -4,96 +4,103 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sys.h"
+#include <time.h>
 #define MAX_THREADS 1000
 
-inline pid_t get_tid() {
-    syscall(SYS_gettid);
-}
+	pid_t get_tid() {
+	    syscall(SYS_gettid);
+	}
 
-int allocated_g;
+	int allocated_g;
 
-int number_of_threads;
-int num_profile_attrs;
-int pre_allocation=500;
-
-
-void check_malloc(void *address, char* message) {
-    if(!address) {
-        printf("**** malloc failed %s", message);
-        exit(0);
-    }
-}
-
-typedef struct thread_stats {
-    long long* timestamps;
-    long int* cmdids;
-    double *profile_attrs;
-    long log_num;
-    struct thread_stats *next;
-    int tid;
-} thread_stats;
-
-__thread struct thread_stats *stats;
-__thread struct thread_stats *current;
-struct thread_stats** thread_stats_g;
-
-extern thread_stats *allocate_thread_stats() {
-    thread_stats* lstats  = malloc(sizeof(thread_stats));
-    check_malloc(lstats,"Allocating stats object failed");
-    lstats->timestamps=malloc(sizeof(long long) * pre_allocation);
-    check_malloc(lstats->timestamps, "Allocating Timestamps");
-    lstats->cmdids=malloc(sizeof(long int)*pre_allocation);
-    check_malloc(lstats->cmdids,"Allocating CMDIDS");
-    lstats->profile_attrs=malloc(sizeof(double)*pre_allocation*num_profile_attrs);
-    check_malloc(lstats->profile_attrs,"Allocating Profile Attributes");
-    lstats->next=0;
-    lstats->log_num=0;
-    return lstats;
-}
+	int number_of_threads;
+	int num_profile_attrs;
+	int pre_allocation=500;
 
 
-//This method needs to be called from Jikes
-/**
- * This method must be called in a thread-safe context.
- * When a new thread is started in JikesRVM, a thread calls this method
- * to initialize its data strcutures and register pointers to its data structure
- */
-extern void register_thread_stat() {
-    stats =  allocate_thread_stats();
-    current = stats;
-    thread_stats_g[number_of_threads]=stats;
-    number_of_threads++;
-}
+	void check_malloc(void *address, char* message) {
+	    if(!address) {
+		printf("**** malloc failed %s", message);
+		exit(0);
+	    }
+	}
 
-void assign_log_entry(double* attrs, long int cmdid, long long timestamp) {
-    //printf("[assign_log_entry] Assigning \n ");
-    current->timestamps[current->log_num]=timestamp;
-    current->cmdids[current->log_num]=cmdid;
-    int profile_start_indx = current->log_num*num_profile_attrs;
-    for(int attr_indx=0; attr_indx < num_profile_attrs;attr_indx++) {
-        current->profile_attrs[profile_start_indx+attr_indx]=attrs[attr_indx];
-    }
+	typedef struct thread_stats {
+	    unsigned long long* timestamps;
+	    long int* cmdids;
+	    double *profile_attrs;
+	    long log_num;
+	    struct thread_stats *next;
+	    int tid;
+	} thread_stats;
 
-    //current->tid = get_tid();
-    current->tid = -1;
-    current->log_num++;
-    //printf("[assign_log_entry] Assigned \n");
-    //stats->log_num++;
-}
+	__thread struct thread_stats *stats;
+	__thread struct thread_stats *current;
+	struct thread_stats** thread_stats_g;
+
+	extern thread_stats *allocate_thread_stats() {
+	    thread_stats* lstats  = malloc(sizeof(thread_stats));
+	    check_malloc(lstats,"Allocating stats object failed");
+	    lstats->timestamps=malloc(sizeof(long long) * pre_allocation);
+	    check_malloc(lstats->timestamps, "Allocating Timestamps");
+	    lstats->cmdids=malloc(sizeof(long int)*pre_allocation);
+	    check_malloc(lstats->cmdids,"Allocating CMDIDS");
+	    lstats->profile_attrs=malloc(sizeof(double)*pre_allocation*num_profile_attrs);
+	    check_malloc(lstats->profile_attrs,"Allocating Profile Attributes");
+	    lstats->next=0;
+	    lstats->log_num=0;
+	    return lstats;
+	}
 
 
-//This method needs to be called from Jikes
-extern void add_log_entry(double* attrs, long int cmdid, long long timestamp) {
-    
-    //printf("[add_log_entry] .... \n");
-    //printf("Current Log Num %d \n",current->log_num);
-    
-    if(current->log_num==pre_allocation) {
-        //printf("[add_log_entry] pre_allocation exceeded. allocating new memoery \n");
-	current->next=allocate_thread_stats();
-        current = current->next;
-    }
-    assign_log_entry(attrs,cmdid,timestamp);
+	//This method needs to be called from Jikes
+	/**
+	 * This method must be called in a thread-safe context.
+	 * When a new thread is started in JikesRVM, a thread calls this method
+	 * to initialize its data strcutures and register pointers to its data structure
+	 */
+	extern void register_thread_stat() {
+	    stats =  allocate_thread_stats();
+	    current = stats;
+	    thread_stats_g[number_of_threads]=stats;
+	    number_of_threads++;
+	}
+
+	void assign_log_entry(double* attrs, long int cmdid, long long timestamp) {
+	    //printf("[assign_log_entry] Assigning \n ");
+	    long            ms; // Milliseconds
+    	    time_t          s;  // Seconds
+    	    struct timespec spec;
+	    s  = spec.tv_sec;
+	    ms = round(spec.tv_nsec / 1.0e6); // Convert nanoseconds to milliseconds
+	    unsigned long long ts = s*1000 + ms;
+	    current->timestamps[current->log_num]=ts;
+	    current->cmdids[current->log_num]=cmdid;
+	    int profile_start_indx = current->log_num*num_profile_attrs;
+	    for(int attr_indx=0; attr_indx < num_profile_attrs;attr_indx++) {
+		current->profile_attrs[profile_start_indx+attr_indx]=attrs[attr_indx];
+	    }
+
+	    current->tid = get_tid();
+	    //current->tid = -1;
+	    current->log_num++;
+	    //printf("[assign_log_entry] Assigned \n");
+	    //stats->log_num++;
+	}
+
+
+	//This method needs to be called from Jikes
+	extern void add_log_entry(double* attrs, long int cmdid, long long timestamp) {
+	    
+	    //printf("[add_log_entry] .... \n");
+	    //printf("Current Log Num %d \n",current->log_num);
+	    
+	    if(current->log_num==pre_allocation) {
+		//printf("[add_log_entry] pre_allocation exceeded. allocating new memoery \n");
+		current->next=allocate_thread_stats();
+		current = current->next;
+	    }
+	    assign_log_entry(attrs,cmdid,timestamp);
 }
 
 extern void print_logs() {
@@ -104,7 +111,7 @@ extern void print_logs() {
         while(thread_stat) {
             int log_indx = 0;
             for(log_indx=0;log_indx < thread_stat->log_num;log_indx++) {
-                printf("%ld,",thread_stat->timestamps[log_indx]);
+                printf("%llu,",thread_stat->timestamps[log_indx]);
                 printf("%d,",thread_stat->tid);
                 printf("%ld,", thread_stat->cmdids[log_indx]);
                 int profile_indx = log_indx*num_profile_attrs;
