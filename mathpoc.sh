335f0f663b9db79bcf7bea90afdbbf04e1ec1eca
Kenan Liu <kliu20@binghamton.edu>
2020-08-18 01:56:52 -0400

    Weird!

---
+++ mathpoc.sh
@@ -0,0 +1,7 @@
+a="5"
+z=$(($a/2))
+if [ "$a" -gt "$z" ]
+then
+	echo "Yeaah"
+fi
+
---
+++ run_all.sh
@@ -0,0 +1,21 @@
+expname=$1
+iters=$2
+
+if [ -d $expname ]
+then
+	rm -r $expname
+fi
+
+mkdir $expname
+
+
+sudo bash run_experiments.sh luindex new $iters 64 2 $expname
+sudo bash run_experiments.sh lusearch new $iters 64 2 $expname
+sudo bash run_experiments.sh jython old $((iters+1)) 64 2 $expname
+sudo bash run_experiments.sh pmd old $((iters+1)) 64 2 $expname
+sudo bash run_experiments.sh xalan old $((iters+1)) 64 2 $expname
+sudo bash run_experiments.sh sunflow new $iters 8 2  $expname
+sudo bash run_experiments.sh fop old $((iters+1)) 1 256 $expname
+sudo bash run_experiments.sh avrora new iters 64 2 $expname
+sudo bash run_experiments.sh hsqldb old $((iters+1)) 256 2 $expname
+
