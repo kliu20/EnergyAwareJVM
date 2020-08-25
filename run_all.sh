b31afc6369c1d3c12602bf9d48962cf614d1a1dd
Kenan Liu <kliu20@binghamton.edu>
2020-08-24 21:17:48 -0400

    New data

--- run_all.sh
+++ run_all.sh
@@ -1,20 +1,21 @@
 expname=$1
 iters=$2
-
+samples="4"
 #if [ -d $expname ]
 #then
 #	rm -r $expname
 #fi
 
-#mkdir $expname
+mkdir $expname
 
 
-#sudo bash run_experiments.sh jython old $((iters+1)) 8 2 $expname
-sudo bash run_experiments.sh pmd old $((iters+1)) 8 2 $expname
-sudo bash run_experiments.sh xalan old $((iters+1)) 8 2 $expname
-sudo bash run_experiments.sh sunflow new $iters 8 2  $expname
-sudo bash run_experiments.sh fop old $((iters+1)) 1 8 $expname
-sudo bash run_experiments.sh avrora new iters 8 2 $expname
-sudo bash run_experiments.sh hsqldb old $((iters+1)) 8 2 $expname
-sudo bash run_experiments.sh lusearch new $iters 8 2 $expname
-sudo bash run_experiments.sh luindex new $iters 8 2 $expname
+sudo bash run_experiments.sh jython old $((iters+1)) 8 2 $expname
+sudo bash run_experiments.sh pmd old $((iters+1)) $samples 2 $expname
+sudo bash run_experiments.sh xalan old $((iters+1)) $samples 2 $expname
+sudo bash run_experiments.sh sunflow new $iters $samples 2  $expname
+sudo bash run_experiments.sh luindex new $iters $samples 2 $expname
+sudo bash run_experiments.sh fop old $((iters+1)) $samples 2 $expname
+sudo bash run_experiments.sh avrora new $iters $samples 2 $expname
+#sudo bash run_experiments.sh lusearch new $iters $samples 2 $expname
+#sudo bash run_experiments.sh hsqldb old $((iters+1)) $samples 2 $expname
+#sudo bash run_experiments.sh jython old $((iters+1)) $samples 2 $expname
--- run_experiments.sh
+++ run_experiments.sh
@@ -1,11 +1,12 @@
 expname=$6
 benchname=$1
-benchdir="kenan_$benchname"
+benchdir="kenan_${benchname}"
 echo "$benchdir"
 bash rapl.sh $1 $2 $3 $4 $5
 bash rapl_on_demand.sh $1 $2 $3 $4 $5
 mkdir $benchdir
-mv counter_based* $benchdir
-mv freq_* $benchdir
+mv counter_based* $benchdir/
+mv freq_* $benchdir/
 bash graph_experiments.sh $benchdir
-mv $benchdir $expname
+cp -r $benchdir $expname/
+rm -r scratch
